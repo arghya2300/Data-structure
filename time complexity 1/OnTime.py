865043179b498f717ddad53c5ed158274f872e15
arghya2300 <arghya.sarkar.bd@gmail.com>
2025-01-28 21:12:15 +0600

    Ontime

---
+++ time complexity 1/OnTime.py
@@ -0,0 +1,13 @@
+def onTime(n):
+    iteration = 0
+    for i in range(1,n+1):  
+        iteration+=1
+    print("when n is", n ,"Iterations = ", iteration)
+
+onTime(10)
+onTime(20)
+onTime(42)
+
+print("\n with every 'n' time taken and iterations will increase linearly")
+
+
