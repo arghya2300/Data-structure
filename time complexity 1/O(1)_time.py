f66068195fe2bad47e4ae9203feba83ba6b0c3e5
arghya2300 <arghya.sarkar.bd@gmail.com>
2025-01-28 21:04:13 +0600

    O(1) Time

---
+++ time complexity 1/O(1)_time.py
@@ -0,0 +1,10 @@
+def printnumber(n):
+    literation = 0
+    print("total number entered by the user is,", n)
+    literation+=1
+    print("total number entered by the computer is,",literation,"\n")
+
+printnumber(10)
+printnumber(200)
+
+print("\n without any 'n' the time taken by the computer will not change")
