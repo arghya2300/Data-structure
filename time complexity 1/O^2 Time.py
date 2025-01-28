0ed395446254bb8d8c2f2ca7dbcf17a9f47709d6
arghya2300 <arghya.sarkar.bd@gmail.com>
2025-01-28 21:18:26 +0600

    O^2 Time

---
+++ time complexity 1/O^2 Time.py
@@ -0,0 +1,16 @@
+def test(n):
+    iteration = 0
+    for i in range(0,n):
+        for j in range(0,n):
+            print("*",end="")
+            iteration+=1
+        print("")
+    print("when n is",n,"iteration is", iteration)
+
+
+test(5)
+test(4)
+test(3)
+
+print("with every 'n' the taken = n^2")
+print("(n^2)")
