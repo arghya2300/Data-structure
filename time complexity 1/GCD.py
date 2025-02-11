e0579984f53c0e41fb74cce526869437a5c63834
arghya2300 <arghya.sarkar.bd@gmail.com>
2025-02-11 21:12:46 +0600

    GCD

---
+++ time complexity 1/GCD.py
@@ -0,0 +1,9 @@
+numberLargest = int(input("Enter the largest number"))
+numberSmallest = int(input("Enter the smallest number"))
+
+while numberSmallest:
+    numberStore = numberSmallest
+    numberSmallest = numberLargest % numberSmallest
+    numberLargest = numberStore
+
+print("HCF is:", numberLargest)
